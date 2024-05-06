6ba2e151a6fda14846553d68f1ecb9a5bc02a26b
Euripedes Landin Moraes Junior <euripedes.junior@ibrowse-sds.com.br>
2024-05-06 14:36:03 -0300

    Criação do docker e conexão do Python com Mysql

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from app_cadastro_kanastra import views
+from django.http import HttpResponse # type: ignore
 
 urlpatterns = [
     path('charges', views.charges, name='charges')
+    path('', lambda x: HttpResponse('Teste Django'))
 ]
---
+++ backend/db/scripts.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS charger;
+USE charger;
+
+CREATE TABLE IF NOT EXISTS charge (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    government_id VARCHAR(11) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    debt_amount DECIMAL(10, 2) NOT NULL,
+    debt_due_date DATE NOT NULL
+);
